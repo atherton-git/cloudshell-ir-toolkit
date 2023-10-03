cc542b031bdcb5f0830edbc7593ef78adb4e395c
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-10-03 23:34:11 +0100

    Create launch.py

---
+++ launch.py
@@ -0,0 +1,44 @@
+import subprocess
+
+clouddrive_dir = "/usr/csuser/clouddrive"
+
+def process_evtx_files():
+    command = [
+        f'{clouddrive_dir}/log-parse-toolkit_v0-1/bin/dotnet-runtime-600/dotnet',
+        f'{clouddrive_dir}/log-parse-toolkit_v0-1/tools/evtx_explorer/EvtxECmd.dll',
+        "-d", f'{clouddrive_dir}/_input/win/',
+        "--csv", f'{clouddrive_dir}/_output/'
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+        print("Command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error code {e.returncode}: {e.stderr}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def process_all_inputs():
+    # Add code to process all inputs here
+    print("Processing all inputs...")
+
+def main():
+    while True:
+        print("Options:")
+        print("1) Process .evtx files")
+        print("2) Process all inputs")
+        print("3) Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            process_evtx_files()
+        elif choice == '2':
+            process_all_inputs()
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
+if __name__ == "__main__":
+    main()
