974d4a6a48fb843f7e6a2af15877ac775b08b98b
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-10-04 00:45:56 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,29 +1,18 @@
 import subprocess
-from scripts.process_evtx import process_evtx_files
 
 clouddrive_dir = "/usr/csuser/clouddrive"
 
-def process_all_inputs():
-    # Add code to process all inputs here
-    print("Processing all inputs...")
+command = [
+    f'./bin/dotnet-runtime-600/dotnet',
+    f'./tools/evtx_explorer/EvtxECmd.dll',
+    "-d", f'./_input/win/',
+    "--csv", f'./_output/'
+]
 
-def main():
-    while True:
-        print("Options:")
-        print("1) Process .evtx files")
-        print("2) Process all inputs")
-        print("3) Quit")
-
-        choice = input("Enter your choice: ")
-
-        if choice == '1':
-            process_evtx_files(clouddrive_dir)
-        elif choice == '2':
-            process_all_inputs()
-        elif choice == '3':
-            break
-        else:
-            print("Invalid choice. Please enter a valid option.")
-
-if __name__ == "__main__":
-    main()
+try:
+    subprocess.run(command, check=True)
+    print("Command executed successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Command failed with error code {e.returncode}: {e.stderr}")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
