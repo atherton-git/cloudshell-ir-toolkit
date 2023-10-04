58569ba9d8063ed6dd42ceb1a122cf6338a8e664
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-10-04 02:25:35 +0100

    Update _launch.py

--- _launch.py
+++ _launch.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 clouddrive_dir = "/usr/csuser/clouddrive"
 
@@ -22,20 +23,34 @@ def process_all_inputs():
     # Add code to process all inputs here
     print("Processing all inputs...")
 
+def parse_linux_log_timestamps():
+    # Run the parse_linux_datetime.py script in the same directory
+    script_path = os.path.join(os.path.dirname(__file__), "parse_linux_datetime.py")
+    try:
+        subprocess.run(["python", script_path], check=True)
+        print("Linux log timestamps parsed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error code {e.returncode}: {e.stderr}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
 def main():
     while True:
         print("Options:")
         print("1) Process .evtx files")
-        print("2) Process all inputs")
-        print("3) Quit")
+        print("2) Parse Linux log timestamps")
+        print("3) Process all inputs")
+        print("4) Quit")
 
         choice = input("Enter your choice: ")
 
         if choice == '1':
             process_evtx_files()
         elif choice == '2':
-            process_all_inputs()
+            parse_linux_log_timestamps()
         elif choice == '3':
+            process_all_inputs()
+        elif choice == '4':
             break
         else:
             print("Invalid choice. Please enter a valid option.")
