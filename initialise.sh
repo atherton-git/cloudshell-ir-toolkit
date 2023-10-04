00b85a6553c816f63dbcc076542085d078bb7912
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-10-04 01:46:33 +0100

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the toolkit directory
-toolkit_dir="/usr/csuser/clouddrive/incident-response-toolkit"
+toolkit_dir="/usr/csuser/clouddrive/cloudshell-ir-toolkit"
 
 # Download and unzip EvtxECmd
 curl -o "$toolkit_dir/tmp/EvtxECmd.zip" "https://f001.backblazeb2.com/file/EricZimmermanTools/net6/EvtxECmd.zip"
@@ -14,4 +14,4 @@ rm -r "$toolkit_dir/tmp/EvtxECmd/"
 curl -o "$toolkit_dir/tmp/dotnet-runtime-6.0.0-linux-x64.tar.gz" "https://download.visualstudio.microsoft.com/download/pr/0ce1c34f-0d9e-4d9b-964e-da676c8e605a/7a6c353b36477fa84f85b2821f2350c2/dotnet-runtime-6.0.0-linux-x64.tar.gz"
 tar -xzvf "$toolkit_dir/tmp/dotnet-runtime-6.0.0-linux-x64.tar.gz" -C "$toolkit_dir/bin/dotnet-runtime-600/"
 
-echo "Setup for log-parse-toolkit has completed."
+echo "Setup has completed."
