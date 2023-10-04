f1ab05ced7af7f267967b21bb329c31e0fc363ba
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-10-04 03:06:32 +0100

    Update parse_linux_datetime.py

--- parse_linux_datetime.py
+++ parse_linux_datetime.py
@@ -5,21 +5,25 @@ from datetime import datetime
 
 # User-defined variables
 log_directory = r"./_input/linux/"
-output_file = r'./_output/'
+output_directory = r'./_output/'
+
+# Update the output_file variable to include the current datetime
+output_filename = datetime.now().strftime("%Y%m%d%H%M%S_linux-log-datetime.csv")
+output_file = os.path.join(output_directory, output_filename)
 
 def process_log_entry(timestamp_string, line, line_number, current_year, relative_path, channel):
     # Check if the log payload has been processed before
     if line.strip() not in processed_payloads:
         timestamp = None
-        formats_to_try = [('%Y %b %d %H:%M:%S', f"{current_year} {timestamp_string}"),  # Example: '2023 Aug 09 00:33:21'
-                          ('%Y %b %d %H:%M:%S.%f', f"{current_year} {timestamp_string}"),  # Example: '2023 Aug 09 00:33:21.915900'
-                          ('%Y-%m-%d %H:%M:%S', timestamp_string),  # Example: '2020-07-07 19:33:19'
-                          ('%d/%b/%Y:%H:%M:%S', timestamp_string)]  # Example: '09/Aug/2023:04:44:03'
+        formats_to_try = [('%Y %b %d %H:%M:%S', f"{current_year} {timestamp_string}"),
+                          ('%Y %b %d %H:%M:%S.%f', f"{current_year} {timestamp_string}"),
+                          ('%Y-%m-%d %H:%M:%S', timestamp_string),
+                          ('%d/%b/%Y:%H:%M:%S', timestamp_string)]
         
         for format_to_try, formatted_timestamp_string in formats_to_try:
             try:
                 timestamp = datetime.strptime(formatted_timestamp_string, format_to_try)
-                break  # Stop trying if one format succeeds
+                break
             except ValueError:
                 pass
 
@@ -36,10 +40,10 @@ current_year = datetime.now().year
 timestamps = []
 processed_payloads = set()
 
-patterns = [r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2})',  # Example: 'Aug 09 00:33:21'
-            r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}\.\d{6})',  # Example: 'Aug 09 00:33:21.915900'
-            r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})',  # Example: '2020-07-07 19:33:19'
-            r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2})']  # Example: '09/Aug/2023:04:44:03'
+patterns = [r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2})',
+            r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}\.\d{6})',
+            r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})',
+            r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2})']
 
 print(f"Searching for log files in {log_directory} and its subdirectories...")
 
@@ -47,7 +51,7 @@ for root, _, files in os.walk(log_directory):
     for file in files:
         current_file = os.path.join(root, file)
         relative_path = os.path.relpath(current_file, log_directory)
-        channel = channel = os.path.splitext(file)[0].split('.')[0]
+        channel = os.path.splitext(file)[0].split('.')[0]
 
         try:
             with open(current_file, 'r', encoding='unicode_escape') as reader:
