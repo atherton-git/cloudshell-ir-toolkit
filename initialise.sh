6a7499938024282a460e51772b345427a0c1d6d2
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-10-04 02:18:03 +0100

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -14,4 +14,7 @@ rm -r "$toolkit_dir/tmp/EvtxECmd/"
 curl -o "$toolkit_dir/tmp/dotnet-runtime-6.0.0-linux-x64.tar.gz" "https://download.visualstudio.microsoft.com/download/pr/0ce1c34f-0d9e-4d9b-964e-da676c8e605a/7a6c353b36477fa84f85b2821f2350c2/dotnet-runtime-6.0.0-linux-x64.tar.gz"
 tar -xzvf "$toolkit_dir/tmp/dotnet-runtime-6.0.0-linux-x64.tar.gz" -C "$toolkit_dir/bin/dotnet-runtime-600/"
 
+# Install pandas for Python
+yes | pip install pandas
+
 echo "Setup has completed."
