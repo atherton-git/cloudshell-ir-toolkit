4a7c2a65dda457b300ac6db929105be924141247
atherton-git <119111530+atherton-git@users.noreply.github.com>
2023-10-25 16:10:40 +0100

    Create parse_qrcodes.py

---
+++ parse_qrcodes.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import pyboof as pb
+
+# Function to process a single image and detect QR codes
+def process_image(image_path):
+    detector = pb.FactoryFiducial(np.uint8).qrcode()
+    image = pb.load_single_band(image_path, np.uint8)
+    detector.detect(image)
+
+    print("Detected a total of {} QR Codes in {}".format(len(detector.detections), image_path))
+    
+    for qr in detector.detections:
+        print("Message: " + qr.message)
+        print("     at: " + str(qr.bounds))
+        print()
+
+# Directory path containing the QR code images
+directory_path = "/home/jack/clouddrive/test"
+
+# List of allowed file extensions
+allowed_extensions = [".png", ".jpg", ".jpeg"]  # Add more extensions if needed
+
+# Iterate through files in the directory
+for filename in os.listdir(directory_path):
+    if any(filename.lower().endswith(ext) for ext in allowed_extensions):
+        image_path = os.path.join(directory_path, filename)
+        process_image(image_path)
