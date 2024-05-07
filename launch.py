6b2202fdbe2c664ac630d5b24710457e6746ccb8
atherton-git <119111530+atherton-git@users.noreply.github.com>
2024-05-07 02:50:48 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,11 +1,16 @@
 import subprocess
 import os
+from datetime import datetime
 
 # Currently unused variable
 clouddrive_dir = "/usr/csuser/clouddrive"
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
 # Change the current working directory
 os.chdir('scripts')
+
+current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
 
 def run_command(command, success_message):
     try:
@@ -31,56 +36,72 @@ def process_evtx_files():
     ]
     run_command(command, "Command executed successfully.")
 
+def search_wordlist():
+    convert = input("Would you like to convert PDF/OpenXML (Microsoft Office) documents to text files? This is is a requirement for bstrings to parse these files (y/n): ").lower().strip()
+    if convert == "y":
+        os.system("python extract_openxml.py")
+        print("OpenXML/PDF documents converted to text files successfully.")
+    elif convert == "n":
+        print("Skipping document conversion.")
+    else:
+        print("Invalid input. Skipping document conversion.")
+    script_path = os.path.join(os.path.dirname(__file__), "scripts", "search_wordlist.sh")
+    os.system(script_path)
+    print("bstrings input searched for wordlist successfully. Please see '_output' directory for results.")
+
 def parse_linux_log_timestamps():
     script_path = os.path.join(os.path.dirname(__file__), "scripts", "parse_linux_datetime.py")
     run_command(["python", script_path], "Linux log timestamps parsed successfully.")
 
 def decode_qrcodes():
     script_path = os.path.join(os.path.dirname(__file__), "scripts", "decode_qrcodes.py")
-    run_command(["python", script_path], "QR codes decoded successfully.")
+    run_command(["python", script_path], "QR codes decoded successfully. Please see '_output' directory for results.")
 
 def search_freetext():
     script_path = os.path.join(os.path.dirname(__file__), "scripts", "search_freesearch.py")
-    run_command(["python", script_path], "Free text search completed.")
+    run_command(["python", script_path], "Free text search completed. Please see '_output' directory for results.")
 
 def search_ipv4():
     script_path = os.path.join(os.path.dirname(__file__), "scripts", "search_ipv4.py")
-    run_command(["python", script_path], "IPv4 search completed.")
+    run_command(["python", script_path], "IPv4 search completed. Please see '_output' directory for results.")
 
 def main():
-    print("""
+	print("""
         __                __     __         ____      _            __              ____   _ __ 
   _____/ /___  __  ______/ /____/ /_  ___  / / /     (_)____      / /_____  ____  / / /__(_) /_
  / ___/ / __ \/ / / / __  / ___/ __ \/ _ \/ / /_____/ / ___/_____/ __/ __ \/ __ \/ / //_/ / __/
 / /__/ / /_/ / /_/ / /_/ (__  ) / / /  __/ / /_____/ / /  /_____/ /_/ /_/ / /_/ / / ,< / / /_  
 \___/_/\____/\__,_/\__,_/____/_/ /_/\___/_/_/     /_/_/         \__/\____/\____/_/_/|_/_/\__/  
                                                                                                
-    """)                                                                                         
-    while True:
-        print("Options:")
-        print("1) Quit")
-        print("2) Log Parsing: Windows Events (EvtxECmd)")
-        print("3) Log Parsing: Linux Timestamps")
-        print("4) Search: Free-text")
-        print("5) Search: IPv4")
-        print("6) Decode: QR codes")
+	""")
+	while True:
+		print("Options:")
+		print("1) Quit")
+		print("2) Log Parsing: Windows Events (EvtxECmd)")
+		print("3) Log Parsing: Linux Timestamps")
+		print("4) Search: bstrings wordlist")
+		print("5) Search: Free-text")
+		print("6) Search: IPv4")
+		print("7) Decode: QR codes")
 
-        choice = input("Enter your choice: ")
+		choice = input("Enter your choice: ")
 
-        if choice == '1':
-            break
-        elif choice == '2':
-            process_evtx_files()
-        elif choice == '3':
-            parse_linux_log_timestamps()
-        elif choice == '4':
-            search_freetext()
-        elif choice == '5':
-            search_ipv4()
-        elif choice == '6':
-            decode_qrcodes()
-        else:
-            print("Invalid choice. Please enter a valid option.")
+		if choice == '1':
+			break
+		elif choice == '2':
+			process_evtx_files()
+		elif choice == '3':
+			parse_linux_log_timestamps()
+		elif choice == '4':
+			search_wordlist()
+		elif choice == '5':
+			search_freetext()
+		elif choice == '6':
+			search_ipv4()
+		elif choice == '7':
+			decode_qrcodes()
+		else:
+			print("Invalid choice. Please enter a valid option.")
 
 if __name__ == "__main__":
-    main()
+	main()
