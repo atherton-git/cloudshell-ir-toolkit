a958a9153091ccb56e125e0694f91510a66372b7
atherton-git <119111530+atherton-git@users.noreply.github.com>
2024-05-07 03:10:49 +0100

    Create search_wordlist.sh

---
+++ scripts/search_wordlist.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+current_datetime=$(date +"%Y%m%d%H%M%S_wordlist.txt")
+cloudshell_dir=$(dirname "$PWD")
+
+$cloudshell_dir/bin/dotnet-runtime-600/dotnet $cloudshell_dir/bin/bstrings/bstrings.dll -q -d "$cloudshell_dir/_input/bstrings" --fs "$cloudshell_dir/bstrings_input.txt" -o "$cloudshell_dir/_output/$current_datetime"
