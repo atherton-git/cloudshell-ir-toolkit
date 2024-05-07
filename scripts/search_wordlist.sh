30c9bd11191380c7e913711a6f6481689a01e9ed
atherton-git <119111530+atherton-git@users.noreply.github.com>
2024-05-07 06:12:31 +0100

    Update search_wordlist.sh

--- scripts/search_wordlist.sh
+++ scripts/search_wordlist.sh
@@ -3,4 +3,4 @@
 current_datetime=$(date +"%Y%m%d%H%M%S_wordlist.txt")
 cloudshell_dir=$(dirname "$PWD")
 
-$cloudshell_dir/bin/dotnet-runtime-600/dotnet $cloudshell_dir/bin/bstrings/bstrings.dll -q -d "$cloudshell_dir/_input/bstrings" --fs "$cloudshell_dir/input_bstrings.txt" -o "$cloudshell_dir/_output/$current_datetime"
+$cloudshell_dir/bin/dotnet-runtime-600/dotnet $cloudshell_dir/bin/bstrings/bstrings.dll -q -d "$cloudshell_dir/_input/bstrings" --fs "$cloudshell_dir/input_wordlist.txt" -o "$cloudshell_dir/_output/$current_datetime"
