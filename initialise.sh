df4ab6ea93b7b9e1844d9635581c57f18ce1bf5e
atherton-git <119111530+atherton-git@users.noreply.github.com>
2024-06-17 17:01:29 +0100

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the toolkit directory
-toolkit_dir="/usr/csuser/clouddrive/cloudshell-ir-toolkit"
+toolkit_dir=$PWD
 
 # Function to calculate hash and compare with expected hash
 check_hash() {
@@ -42,14 +42,6 @@ unzip "$toolkit_dir/tmp/EvtxECmd.zip" -d "$toolkit_dir/tmp/EvtxECmd/"
 mv "$toolkit_dir/tmp/EvtxECmd/EvtxECmd/"* "$toolkit_dir/bin/evtx_explorer/"
 rm -r "$toolkit_dir/tmp/EvtxECmd/"
 
-# bstrings Installation
-curl -o "$toolkit_dir/tmp/bstrings.zip" "https://f001.backblazeb2.com/file/EricZimmermanTools/net6/bstrings.zip"
-bstrings_expected_hash="1521031bab2843757bb701b75741a24154965ba219a57cbfefddb792c6d5b301"
-check_hash "$toolkit_dir/tmp/bstrings.zip" "$bstrings_expected_hash"
-unzip "$toolkit_dir/tmp/bstrings.zip" -d "$toolkit_dir/tmp/bstrings/"
-mv "$toolkit_dir/tmp/bstrings/"* "$toolkit_dir/bin/bstrings/"
-rm -r "$toolkit_dir/tmp/bstrings/"
-
 # Download dotnet runtime
 curl -o "$toolkit_dir/tmp/dotnet-runtime-6.0.0-linux-x64.tar.gz" "https://download.visualstudio.microsoft.com/download/pr/0ce1c34f-0d9e-4d9b-964e-da676c8e605a/7a6c353b36477fa84f85b2821f2350c2/dotnet-runtime-6.0.0-linux-x64.tar.gz"
 dotnet_expected_hash="1a4076139944f3b16d9a0fc4841190cf060a9d93ebc13330821a2e97f6d4db91"
@@ -57,13 +49,10 @@ check_hash "$toolkit_dir/tmp/dotnet-runtime-6.0.0-linux-x64.tar.gz" "$dotnet_exp
 tar -xzvf "$toolkit_dir/tmp/dotnet-runtime-6.0.0-linux-x64.tar.gz" -C "$toolkit_dir/bin/dotnet-runtime-600/"
 
 # Install Python modules
-pip install pandas
-pip install pyboof
-pip install numpy
-pip install pyarrow
-pip install python-docx
-pip install python-pptx
-pip install openpyxl
-pip install PyPDF2
+yes | pip install pandas --break-system-packages
+yes | pip install pyboof --break-system-packages
+yes | pip install numpy --break-system-packages
+yes | pip install pyarrow --break-system-packages
+yes | pip install tika --break-system-packages
 
 echo "Setup has completed."
