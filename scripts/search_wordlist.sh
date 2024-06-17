af3fc4d3cae725645c22d3e101d57a2f626670ff
atherton-git <119111530+atherton-git@users.noreply.github.com>
2024-06-17 17:07:20 +0100

    Delete scripts/search_wordlist.sh

--- scripts/search_wordlist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-current_datetime=$(date +"%Y%m%d%H%M%S_wordlist.txt")
-cloudshell_dir=$(dirname "$PWD")
-
-$cloudshell_dir/bin/dotnet-runtime-600/dotnet $cloudshell_dir/bin/bstrings/bstrings.dll -q -d "$cloudshell_dir/_input/bstrings" --fs "$cloudshell_dir/input_wordlist.txt" -o "$cloudshell_dir/_output/$current_datetime"
